dc210dd05bc57cd11fdc4af05e081d8f2d985056
Dan <daniel.barela@gmail.com>
2014-08-11 14:55:45 -0600

    Create caskconfig.sh

---
+++ caskconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew tap caskroom/cask
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install skitch
+brew cask install the-unarchiver
+brew cask install android-studio
+brew cask install atom
+brew cask install firefox
+brew cask install rightzoom
+brew cask install google-earth
