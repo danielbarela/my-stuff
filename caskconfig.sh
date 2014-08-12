6fd6366d9998460ed1f1d892947e0ac7bf4b66e2
Dan <daniel.barela@gmail.com>
2014-08-12 10:13:59 -0600

    Update caskconfig.sh

--- caskconfig.sh
+++ caskconfig.sh
@@ -2,6 +2,7 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew tap caskroom/cask
 brew install caskroom/cask/brew-cask
+brew cask install mongodb
 brew cask install google-chrome
 brew cask install skitch
 brew cask install the-unarchiver
